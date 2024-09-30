6646180da3dd35732ee927998ba3a4d8983244b6
victoragrela <victoragrela2@gmail.com>
2024-09-30 20:35:04 -0300

    página de cálculo estilizada

--- my-app/src/pages/calculation.tsx
+++ my-app/src/pages/calculation.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image'; // Importe o componente Image
 
 export default function Calculation() {
   const [result, setResult] = useState<number | null>(null);
@@ -27,7 +28,7 @@ export default function Calculation() {
 
         <button 
           onClick={calculate} 
-          className="bg-blue-500 text-white p-2 rounded transition-[0.25s] hover:bg-[#005c99]"
+          className="bg-blue-500 text-white p-2 rounded transition-[0.25s] hover:bg-[#005c99] mb-8"
         >
           Realizar Cálculo
         </button>
@@ -37,6 +38,28 @@ export default function Calculation() {
             <p className="text-xl">Resultado do Cálculo: {result.toFixed(2)}</p>
           </div>
         )}
+        
+        <p className="mb-8">
+          Os cálculos e previsões de Immanuel Velikovsky, especialmente em "Worlds in Collision", são centrais para as controvérsias que cercam seu trabalho. Velikovsky usou um método que mesclava observações astronômicas, interpretações mitológicas e relatos históricos para fundamentar suas alegações de que eventos cataclísmicos ocorreram de maneira muito mais recente do que a ciência aceita.
+        </p>
+        <p className="mb-8">
+          Um dos pontos mais contestados foi sua interpretação do movimento dos planetas. Velikovsky sugeriu que Vênus, ao ser ejetado de Júpiter, passou próximo à Terra, resultando em uma série de catástrofes naturais. Ele se apoiou em textos mitológicos e relatos antigos para fundamentar suas teorias, mas seus métodos de cálculo não foram considerados rigorosos por muitos cientistas. A falta de evidências empíricas para sustentar suas alegações astronômicas levou a um ceticismo generalizado.
+        </p>
+        <p className="mb-8">
+          A resistência à sua obra se intensificou devido à natureza radical de suas afirmações, que desafiavam não apenas a astronomia, mas também a geologia e a história. A comunidade científica argumentou que suas previsões sobre as consequências dos eventos cataclísmicos eram inconsistentes com a compreensão estabelecida dos processos geológicos e astronômicos. Por exemplo, a ideia de que a Terra poderia ter sofrido impactos cataclísmicos em escalas de tempo tão curtas não encontra suporte nas evidências geológicas.
+        </p>
+        
+        <Image 
+          src="/assets/immanuel_carl.png"
+          alt="Carl Sagan com Immanuel Velikovsky"
+          width={300}
+          height={100}
+          className='mb-8'
+        />
+        
+        <p className="mb-8">
+          A controvérsia em torno das teorias de Velikovsky gerou um debate mais amplo sobre o papel da ciência, da mitologia e da história. Embora suas ideias tenham sido desacreditadas em muitos círculos, elas continuam a suscitar interesse, especialmente entre aqueles que buscam explicações alternativas para os eventos da história da Terra. Velikovsky se tornou uma figura emblemática no estudo de teorias não convencionais, lembrando-nos da complexidade da busca por entender o nosso lugar no universo.
+        </p>
       </div>
     </div>
   );
